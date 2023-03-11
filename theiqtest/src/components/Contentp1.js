4782c6b7f9c0e94978270657b313cbf6698e1430
Aman <yoab100203@gmail.com>
2023-03-11 14:26:02 +0530

    Adding simple components

---
+++ theiqtest/src/components/Contentp1.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Contentp1() {
+  return (
+    <div>Contentp1</div>
+  )
+}
---
+++ theiqtest/src/components/Headerp1.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Headerp1() {
+  return (
+    <div>Headerp1</div>
+  )
+}
